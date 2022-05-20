0a972b103a267336f8cea8fbcff0317b067ec73c
EvgeniyaZay <zhenyayaya121996@gmail.com>
2022-05-20 23:50:53 +0300

    финал

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -105,7 +105,8 @@ def post_edit(request, post_id):
     form = PostForm(
                     request.POST,
                     files=request.FILES or None,
-                    instance=post)
+                    instance=post
+    )
     if not form.is_valid():
         return render(request, 'posts/create_post.html', {
             'form': form,
